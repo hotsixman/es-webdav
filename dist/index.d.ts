9496d919234855aaa707c9e53224064784b0f3bd
HotsixMan <hotsixman05@gmail.com>
2025-03-13 00:33:23 +0900

    version: 0.0.4

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,9 @@
-export {};
+import { WebdavServer } from "./module/webdav-server.js";
+import * as func from './module/func.js';
+import { ExpectedError } from "./module/expected-error.js";
+import type { AuthInterface } from "./module/manager/auth-manager.js";
+import type { ResourceLockInterface } from "./module/manager/resource-lock-manager.js";
+export { WebdavServer, func, ExpectedError };
+export type { AuthInterface, ResourceLockInterface };
+export default WebdavServer;
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
-export {};
+import { WebdavServer } from "./module/webdav-server.js";
+import * as func from './module/func.js';
+import { ExpectedError } from "./module/expected-error.js";
+export { WebdavServer, func, ExpectedError };
+export default WebdavServer;
 //# sourceMappingURL=index.js.map
